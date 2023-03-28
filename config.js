191cb9f9d8c2f6faa9ea1c7483fa722a84255e20
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-28 22:28:27 +0800

    config express

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    channelAccessToken: 'Igg5OXcdDw7SGZ22j2Cg/2SJFHgCpKjc69QKn4byz+W45pXrmamqDwiT1aVkAxSwUcG+/eTrsU/VL2spyB4oM/5podn+SsNRbvHx7LgICy6jSBdrIMnKBloy9K9ZmMcrzhknwRBl3RqWemmuCGyNlQdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '89ddea49b1c340ca26c32538f4e09614'
+};
+
+module.exports = config;
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const { Configuration, OpenAIApi } = require("openai");
+const line = require('@line/bot-sdk');
+const express = require('express');
 const readlineSync = require("readline-sync");
-require("dotenv").config();
 
+require("dotenv").config();
 
 //-------------------- Initialize --------------------//
 
@@ -11,11 +13,11 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const history = [];
-
 //-------------------- Request --------------------//
 
 const chatCompletion = async () => {
+    const history = [];
+
     while (true) {
         const user_input = readlineSync.question("Your input: ");
 
@@ -75,4 +77,47 @@ const creatImage = async () => {
     console.log(`Here's your image's URL:\n${response.data.data[0].url}`)
 }
 
-chatCompletion()
+/*  Line  */
+const config = {
+    channelAccessToken: 'Igg5OXcdDw7SGZ22j2Cg/2SJFHgCpKjc69QKn4byz+W45pXrmamqDwiT1aVkAxSwUcG+/eTrsU/VL2spyB4oM/5podn+SsNRbvHx7LgICy6jSBdrIMnKBloy9K9ZmMcrzhknwRBl3RqWemmuCGyNlQdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '89ddea49b1c340ca26c32538f4e09614'
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+const app = express();
+
+// register a webhook handler with middleware
+app.post('/callback', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
+});
+
+// event handler
+function handleEvent(event) {
+
+    // ignore non-text-message event
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
