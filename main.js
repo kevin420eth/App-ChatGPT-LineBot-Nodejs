8a83bc547ef6ca4c815c845c4b1ae5135c96dc7d
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-29 14:43:49 +0800

    modified reply

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ const app = express();
 // register a webhook handler with middleware
 app.post('/callback', line.middleware(config), (req, res) => {
     Promise
-        .all(req.body.events.map(handleEvent))
+        .ther(req.body.events.map(handleEvent))
         .then((result) => res.json(result))
         .catch((err) => {
             console.error(err);
