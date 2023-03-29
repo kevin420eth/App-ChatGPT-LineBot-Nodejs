bc3218d765ce73992d2de675debe81ce5d7826aa
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-29 14:39:15 +0800

    modified reply

--- main.js
+++ main.js
@@ -103,9 +103,8 @@ function handleEvent(event) {
         return Promise.resolve(null);
     }
 
-    const echo = { type: 'text', text: event.message.text };
     // create a echoing text message
-    const reply = chatCompletion(event.message.text)
+    const reply = {type: 'text', text:chatCompletion(event.message.text)}
 
     // use reply API
     return client.replyMessage(event.replyToken, reply);
