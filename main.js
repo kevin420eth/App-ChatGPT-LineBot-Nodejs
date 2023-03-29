819d2503db3ce3d91b09b2eba0972fe41b590493
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-29 14:28:01 +0800

    modified reply message

--- main.js
+++ main.js
@@ -18,51 +18,45 @@ const openai = new OpenAIApi(configuration);
 const chatCompletion = async (line_message) => {
     const history = [];
 
-    while (true) {
-        const user_input = line_message
+    const user_input = line_message
 
-        if (user_input.toLowerCase() === 'exit') {
-            break
-        }
+    const messages = [];
 
-        const messages = [];
+    for (const [input_text, completion_text] of history) {
+        messages.push({ role: "user", content: input_text });
+        messages.push({ role: "assistant", content: completion_text });
+    }
 
-        for (const [input_text, completion_text] of history) {
-            messages.push({ role: "user", content: input_text });
-            messages.push({ role: "assistant", content: completion_text });
-        }
+    messages.push({ role: "user", content: user_input });
 
-        messages.push({ role: "user", content: user_input });
+    try {
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+            temperature: 1,
+            top_p: 1,
+            n: 1,
+            presence_penalty: 0,
+            frequency_penalty: 0,
+            //stream: false,
+            //stop: null,
+            //max_tokens: Infinity,
+            //logit_bias:null,
+            //user:""
+        })
+        const completion_text = response.data.choices[0].message.content
 
-        try {
-            const response = await openai.createChatCompletion({
-                model: "gpt-3.5-turbo",
-                messages: messages,
-                temperature: 1,
-                top_p: 1,
-                n: 1,
-                presence_penalty: 0,
-                frequency_penalty: 0,
-                //stream: false,
-                //stop: null,
-                //max_tokens: Infinity,
-                //logit_bias:null,
-                //user:""
-            })
-            const completion_text = response.data.choices[0].message.content
+        history.push([user_input, completion_text]);
 
-            history.push([user_input, completion_text]);
+        console.log(completion_text)
+        return completion_text
 
-            console.log(completion_text)
-            return completion_text
-
-        } catch (error) {
-            if (error.response) {
-                console.log(error.response.status);
-                console.log(error.response.data);
-            } else {
-                console.log(error.message);
-            }
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
         }
     }
 }
@@ -119,7 +113,7 @@ function handleEvent(event) {
 //Confirm working status
 app.get('/', (req, res) => {
     res.send('ChatGPT is listening...');
-  });
+});
 
 // listen on port
 const port = process.env.PORT || 3000;
